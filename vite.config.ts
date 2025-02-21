171d64c2b7ffa0d609b334d3c8b2dd43998138f0
phi.hoang.nguyen <phih5066@gmail.com>
2025-02-21 15:00:58 +0700

    fix: change tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     allowedHosts: [],
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
